6775b7f0c57f1ada1d31d24600db4b89ec53bebc
Olexandr Ivaschenko <sasaivasenko9@gmail.com>
2025-04-25 19:31:18 +0300

    create table form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,226 @@
 'use strict';
 
-// write code here
+const headers = document.querySelectorAll('thead tr th');
+
+headers.forEach((header) => {
+  header.addEventListener('click', () => {
+    const table = document.querySelector('table');
+    const rows = Array.from(table.querySelectorAll('tbody tr'));
+    const index = Array.from(header.parentNode.children).indexOf(header);
+    const isAscending = header.classList.toggle('ascending');
+    const sortedRows = rows.sort((a, b) => {
+      const aText = a.querySelectorAll('td')[index].textContent;
+      const bText = b.querySelectorAll('td')[index].textContent;
+
+      if (isAscending) {
+        if (!isNaN(Number(aText)) && !isNaN(Number(bText))) {
+          return parseFloat(aText) - parseFloat(bText);
+        }
+
+        return aText.localeCompare(bText);
+      } else {
+        if (!isNaN(Number(aText)) && !isNaN(Number(bText))) {
+          return parseFloat(bText) - parseFloat(aText);
+        }
+
+        return bText.localeCompare(aText);
+      }
+    });
+    const tbody = table.querySelector('tbody');
+
+    tbody.innerHTML = '';
+    sortedRows.forEach((row) => tbody.appendChild(row));
+  });
+});
+
+const tr = document.querySelectorAll('tbody tr');
+let prevSelectedRow = document.querySelector('tbody tr.selected');
+
+tr.forEach((row) => {
+  row.addEventListener('click', () => {
+    if (prevSelectedRow) {
+      prevSelectedRow.classList.remove('active');
+    }
+    row.classList.add('active');
+    prevSelectedRow = row;
+  });
+});
+
+const form = document.createElement('form');
+
+form.classList.add('new-employee-form');
+
+const nameLabel = document.createElement('label');
+
+nameLabel.textContent = 'Name:';
+
+const nameInput = document.createElement('input');
+
+nameInput.type = 'text';
+nameInput.name = 'name';
+nameInput.setAttribute('data-qa', 'name');
+nameInput.setAttribute('required', 'true');
+nameLabel.appendChild(nameInput);
+
+const positionLabel = document.createElement('label');
+
+positionLabel.textContent = 'Position:';
+
+const positionInput = document.createElement('input');
+
+positionInput.type = 'text';
+positionInput.name = 'position';
+positionInput.setAttribute('data-qa', 'position');
+positionInput.setAttribute('required', 'true');
+positionLabel.appendChild(positionInput);
+
+const officeLabel = document.createElement('label');
+
+officeLabel.textContent = 'Office:';
+
+const officeInput = document.createElement('select');
+const officeOptions = [
+  'Tokyo',
+  'Singapore',
+  'London',
+  'New York',
+  'Edinburgh',
+  'San Francisco',
+];
+
+officeInput.name = 'office';
+officeInput.setAttribute('data-qa', 'office');
+officeInput.setAttribute('required', 'true');
+
+officeOptions.forEach((option) => {
+  const opt = document.createElement('option');
+
+  opt.value = option;
+  opt.textContent = option;
+  officeInput.appendChild(opt);
+});
+officeLabel.appendChild(officeInput);
+
+const ageLabel = document.createElement('label');
+
+ageLabel.textContent = 'Age:';
+
+const ageInput = document.createElement('input');
+
+ageInput.type = 'number';
+ageInput.name = 'age';
+ageInput.setAttribute('data-qa', 'age');
+ageInput.setAttribute('required', 'true');
+ageLabel.appendChild(ageInput);
+
+const salaryLabel = document.createElement('label');
+
+salaryLabel.textContent = 'Salary:';
+
+const salaryInput = document.createElement('input');
+
+salaryInput.type = 'number';
+salaryInput.name = 'salary';
+salaryInput.setAttribute('data-qa', 'salary');
+salaryInput.setAttribute('required', 'true');
+salaryLabel.appendChild(salaryInput);
+
+const addButton = document.createElement('button');
+
+addButton.textContent = 'Add Employee';
+addButton.type = 'submit';
+form.appendChild(nameLabel);
+form.appendChild(positionLabel);
+form.appendChild(officeLabel);
+form.appendChild(ageLabel);
+form.appendChild(salaryLabel);
+form.appendChild(addButton);
+document.body.appendChild(form);
+
+const addEmployee = (e) => {
+  e.preventDefault();
+
+  const names = nameInput.value;
+  const position = positionInput.value;
+  const office = officeInput.value;
+  const age = Number(ageInput.value);
+  const salary = `$${parseFloat(salaryInput.value).toLocaleString('en-US')}`;
+
+  const newRow = document.createElement('tr');
+
+  newRow.innerHTML = `
+    <td>${names}</td>
+    <td>${position}</td>
+    <td>${office}</td>
+    <td>${age}</td>
+    <td>${salary}</td>
+  `;
+
+  const notification = document.createElement('div');
+
+  notification.classList.add('notification');
+  notification.setAttribute('data-qa', 'notification');
+
+  if (names.length < 4) {
+    notification.textContent = 'Name must be at least 4 characters long';
+    notification.classList.add('error');
+  } else if (age < 18 || age > 90) {
+    notification.textContent = 'Age must be at least 18';
+    notification.classList.add('error');
+  } else {
+    notification.textContent = 'Employee added successfully';
+    notification.classList.add('success');
+    document.querySelector('tbody').appendChild(newRow);
+  }
+  notification.style.position = 'fixed';
+  notification.style.top = '50%';
+  notification.style.right = '50%';
+  document.body.appendChild(notification);
+
+  setTimeout(() => {
+    notification.remove();
+  }, 3000);
+};
+
+form.addEventListener('submit', addEmployee);
+
+let dobleClick = false;
+
+tr.forEach((row, rowIndex) => {
+  row.addEventListener('dblclick', () => {
+    const cells = row.querySelectorAll('td');
+
+    const saveCell = {};
+
+    if (dobleClick === false) {
+      cells.forEach((cell, cellIndex) => {
+        const input = document.createElement('input');
+
+        input.classList.add('cell-input');
+        saveCell[cellIndex] = cell.textContent;
+
+        input.type = 'text';
+        input.value = cell.textContent;
+        cell.innerHTML = '';
+        cell.appendChild(input);
+      });
+      dobleClick = true;
+    }
+
+    row.addEventListener('keydown', (e) => {
+      if (e.key === 'Enter') {
+        cells.forEach((cell, cellIndex) => {
+          const input = cell.querySelector('input');
+          const inputValue = input.value.trim();
+
+          if (input && inputValue !== '') {
+            cell.textContent = input.value;
+          } else {
+            cell.textContent = saveCell[cellIndex];
+          }
+        });
+        dobleClick = false;
+      }
+    });
+  });
+});
